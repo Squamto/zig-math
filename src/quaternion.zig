d66dc74137ca8105b0f86448721254ab6d827a68
Squamto <25984337+Squamto@users.noreply.github.com>
2024-09-21 20:41:23 +0200

    QuatToMat

--- src/quaternion.zig
+++ src/quaternion.zig
@@ -3,6 +3,7 @@ const std = @import("std");
 
 const Vec3 = @import("vector.zig").Vec3;
 const Vec4 = @import("vector.zig").Vec4;
+const Mat4 = @import("matrix.zig").QuadMatrix(f32, 4);
 
 pub const Quat4 = struct {
     data: @Vector(4, f32) = @splat(0),
@@ -24,21 +25,6 @@ pub const Quat4 = struct {
     }
 
     pub fn mul(lhs: Quat4, rhs: Quat4) Quat4 {
-        //QuatMul(QUAT *q1, QUAT *q2, QUAT *res){
-        //float A, B, C, D, E, F, G, H;
-        //A = (q1->w + q1->x)*(q2->w + q2->x);
-        //B = (q1->z - q1->y)*(q2->y - q2->z);
-        //C = (q1->w - q1->x)*(q2->y + q2->z);
-        //D = (q1->y + q1->z)*(q2->w - q2->x);
-        //E = (q1->x + q1->z)*(q2->x + q2->y);
-        //F = (q1->x - q1->z)*(q2->x - q2->y);
-        //G = (q1->w + q1->y)*(q2->w - q2->z);
-        //H = (q1->w - q1->y)*(q2->w + q2->z);
-        //res->w = B + (-E - F + G + H) /2;
-        //res->x = A - (E + F + G + H)/2;
-        //res->y = C + (E - F + G - H)/2;
-        //res->z = D + (E - F - G + H)/2;
-
         const A = (lhs.data[3] + lhs.data[0]) * (rhs.data[3] + rhs.data[0]);
         const B = (lhs.data[2] - lhs.data[1]) * (rhs.data[1] - rhs.data[2]);
         const C = (lhs.data[3] - lhs.data[0]) * (rhs.data[1] + rhs.data[2]);
@@ -93,6 +79,50 @@ pub const Quat4 = struct {
         const qvec = Quat4.from_vec(vec);
         const result = self.mul(qvec).mul(self.conjugate());
         return Vec3.init(.{ result.data[0], result.data[1], result.data[2] });
+    }
+
+    pub fn to_matrix(self: Quat4) Mat4 {
+        const x = self.data[0];
+        const y = self.data[1];
+        const z = self.data[2];
+        const w = self.data[3];
+
+        const xx = x * x;
+        const xy = x * y;
+        const xz = x * z;
+        const xw = x * w;
+        const yy = y * y;
+        const yz = y * z;
+        const yw = y * w;
+        const zz = z * z;
+        const zw = z * w;
+
+        return Mat4{ .data = .{
+            Vec4.init(.{
+                1.0 - 2.0 * (yy + zz),
+                2.0 * (xy - zw),
+                2.0 * (xz + yw),
+                0.0,
+            }),
+            Vec4.init(.{
+                2.0 * (xy + zw),
+                1.0 - 2.0 * (xx + zz),
+                2.0 * (yz - xw),
+                0.0,
+            }),
+            Vec4.init(.{
+                2.0 * (xz - yw),
+                2.0 * (yz + xw),
+                1.0 - 2.0 * (xx + yy),
+                0.0,
+            }),
+            Vec4.init(.{
+                0.0,
+                0.0,
+                0.0,
+                1.0,
+            }),
+        } };
     }
 
     test norm {
@@ -227,5 +257,32 @@ pub const Quat4 = struct {
         try std.testing.expectApproxEqAbs(expected.data[0], result.data[0], epsilon);
         try std.testing.expectApproxEqAbs(expected.data[1], result.data[1], epsilon);
         try std.testing.expectApproxEqAbs(expected.data[2], result.data[2], epsilon);
+    }
+
+    test to_matrix {
+        const epsilon = std.math.floatEps(f32);
+        const quat = Quat4.form_euler(Vec3.init(.{ 0.0, 0.0, std.math.pi }));
+        var expected = Mat4.identity(1.0);
+        expected.set(0, 0, -1.0);
+        expected.set(1, 1, -1.0);
+
+        const result = quat.to_matrix();
+
+        try std.testing.expectApproxEqAbs(expected.get(0, 0), result.get(0, 0), epsilon);
+        try std.testing.expectApproxEqAbs(expected.get(0, 1), result.get(0, 1), epsilon);
+        try std.testing.expectApproxEqAbs(expected.get(0, 2), result.get(0, 2), epsilon);
+        try std.testing.expectApproxEqAbs(expected.get(0, 3), result.get(0, 3), epsilon);
+        try std.testing.expectApproxEqAbs(expected.get(1, 0), result.get(1, 0), epsilon);
+        try std.testing.expectApproxEqAbs(expected.get(1, 1), result.get(1, 1), epsilon);
+        try std.testing.expectApproxEqAbs(expected.get(1, 2), result.get(1, 2), epsilon);
+        try std.testing.expectApproxEqAbs(expected.get(1, 3), result.get(1, 3), epsilon);
+        try std.testing.expectApproxEqAbs(expected.get(2, 0), result.get(2, 0), epsilon);
+        try std.testing.expectApproxEqAbs(expected.get(2, 1), result.get(2, 1), epsilon);
+        try std.testing.expectApproxEqAbs(expected.get(2, 2), result.get(2, 2), epsilon);
+        try std.testing.expectApproxEqAbs(expected.get(2, 3), result.get(2, 3), epsilon);
+        try std.testing.expectApproxEqAbs(expected.get(3, 0), result.get(3, 0), epsilon);
+        try std.testing.expectApproxEqAbs(expected.get(3, 1), result.get(3, 1), epsilon);
+        try std.testing.expectApproxEqAbs(expected.get(3, 2), result.get(3, 2), epsilon);
+        try std.testing.expectApproxEqAbs(expected.get(3, 3), result.get(3, 3), epsilon);
     }
 };
